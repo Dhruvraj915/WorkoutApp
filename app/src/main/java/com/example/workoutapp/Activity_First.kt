72ed8cb90d2aad0bbd14b0e64a6521b233daf113
Dhruvraj915 <dhruvraj0915@gmail.com>
2025-03-18 17:37:23 +0530

    basic flow completed

--- app/src/main/java/com/example/workoutapp/Activity_First.kt
+++ app/src/main/java/com/example/workoutapp/Activity_First.kt
@@ -1,4 +1,30 @@
 package com.example.workoutapp
 
-class Activity_First {
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Activity_First : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+        val fullWorkoutButton: Button = findViewById(R.id.button3)
+
+        fullWorkoutButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/UIPvIYsjfpo?si=mXA9W_bTwnCTt9nP"))
+            startActivity(intent)
+        }
+
+        val startWorkoutButton: Button = findViewById(R.id.button2)
+
+        startWorkoutButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/UIPvIYsjfpo?si=mXA9W_bTwnCTt9nP"))
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.workoutapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.HorizontalScrollView
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,7 +22,14 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            return@setOnApplyWindowInsetsListener insets
+        }
+
+        val imageView: ImageView = findViewById(R.id.imageView5)
+        imageView.setOnClickListener {
+            val intent = Intent(this, Activity_First::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
